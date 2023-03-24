5bacdd1c4f5bde7ade8799cdef92785b04583465
Samuel Iansen <iansen2022@gmail.com>
2023-03-23 22:20:39 -0300

    programa teclado_produto

---
+++ programa.java
@@ -0,0 +1,14 @@
+package aplicacao;
+
+import java.util.Scanner;
+
+public class programa {
+
+	public static void main(String[] args) {
+		
+		Scanner teclado = new Scanner (System.in);
+		
+		produtos produto = new produtos ();
+		
+		}
+	}
